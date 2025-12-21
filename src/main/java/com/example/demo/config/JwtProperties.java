7e9db6abef1ce764baad55d69227b28d9dcd86f4
bala-amypo <bala@amypo.in>
2025-12-21 16:13:33 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtProperties.java
+++ src/main/java/com/example/demo/config/JwtProperties.java
@@ -1,3 +1,10 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Getter;
+import lombok.Setter;
+
 @Component
 @ConfigurationProperties(prefix = "jwt")
 @Getter

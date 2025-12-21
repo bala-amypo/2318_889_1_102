43ae4d20c28f2e561895d346a03ed58364210797
bala-amypo <bala@amypo.in>
2025-12-21 15:00:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handle(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
